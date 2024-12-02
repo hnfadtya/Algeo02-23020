170ccd0ca739eea9cfe3bec64f83d246a3df9f4e
Hanif Kalyana Aditya <19623120@std.stei.itb.ac.id>
2024-12-02 14:31:04 +0700

    refactor

--- src/backend/image_retrieval/retrieval.py
+++ src/backend/image_retrieval/retrieval.py
@@ -8,7 +8,7 @@ from backend.image_retrieval.similarity import compute_similarity
 def image_retrieval_function(image_file):
     # Proses gambar menjadi vektor numpy
     folder_path = 
-    totalImage = getTotalImage(folder_path)
+    totalImages = getTotalImage(folder_path)
     size = (100,100)
     intensityMatrix = []
     filenames = []
@@ -18,13 +18,14 @@ def image_retrieval_function(image_file):
             try:
                 filenames.append(filename)
                 intensityMatrix.append(process_image(full_path, size))
-                # Standarisasi dataset gambar
-                standardized_images, mean_vector = standardize_images(intensityMatrix) #image_vectors[i] adalah intensitas pixel
             except Exception as e:
                 print(f"Error processing {filename}: {e}")
     
+    # Standarisasi dataset gambar
+    standardized_images = standardize_images(intensityMatrix) #standardized_images adalah matrix dari seluruh gambar yang telah distandarisasi, size = MN x MN
+
     # Komputasi PCA
-    _, _, Vt_k = compute_pca(standardized_images, num_components=50)
+    _, _, Vt_k = compute_pca(standardized_images, num_components=50, totalImages)
     # Proyeksi query ke ruang PCA
     query_projection = np.dot(image_vector - mean_vector, Vt_k.T)
     # Hitung kesamaan antara query dan dataset
