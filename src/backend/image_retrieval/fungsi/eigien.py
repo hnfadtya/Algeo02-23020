aa11f07ed63434c86b63c6b527c3844409fd1be8
19623023 <19623023@std.stei.itb.ac.id>
2024-12-04 09:23:55 +0700

    Egien added

--- src/backend/image_retrieval/fungsi/eigien.py
+++ src/backend/image_retrieval/fungsi/eigien.py
@@ -1,27 +1,30 @@
 import numpy as np
-from numpy import array
 
-# Buat Matriks Identitas NxN
-def matrixIdentitas(N):
-    return np.eye(N)
-
-# Menghitung determinan
-def determinan(matrix):
-    return np.linalg.det(matrix)
-
-# Mendapatkan Nilai Eigen dengan menggunakan persamaan karakteristik
 def eigenValues(matrix):
-    coefficients = np.poly(matrix)
-    eigenValues = np.roots(coefficients)
-    
-    return np.unique((eigenValues)).astype(int)
-    # return np.unique(np.round(eigenValues, 2)).astype(int)
+    # Memeriksa apakah matriks adalah persegi (M x N)
+    if matrix.shape[0] == matrix.shape[1]:
+        # Untuk matriks persegi, kita dapat menghitung nilai eigen secara langsung
+        coefficients = np.poly(matrix)
+        eigenValues = np.roots(coefficients)
+        return np.unique(eigenValues).astype(float)
+    else:
+        # Matriks non-persegi
+        AtA = np.dot(matrix.T, matrix)  # Matriks A^T A (N x N)
+        AAt = np.dot(matrix, matrix.T)  # Matriks A A^T (M x M)
 
-matrix = array([[3, -2, 0], [-2, 3, 0], [0, 0, 5]])
-eigen = eigenValues(matrix)
-print(eigen)
-# det = determinan(matrix)
-# identitas = matrixIdentitas(3)
-# print("Determinannya adalah:", det)
-# print(identitas)
+        # Menghitung eigenvalues dari A^T A atau A A^T
+        eigenvalues_AtA = np.linalg.eigvals(AtA)
+        
+        # Nilai singular
+        singular_values = np.sqrt(np.abs(eigenvalues_AtA))
+        return np.sort(singular_values)
 
+# Contoh matriks persegi (2x2)
+matrix_square = np.array([[3, -2, 0], [-2, 3, 0], [0, 0, 5]])
+eigen_square = eigenValues(matrix_square)
+print("Eigen values (persegi):", eigen_square)
+
+# Contoh matriks non-persegi (3x2)
+matrix_non_square = np.array([[1, 1], [0, 1], [1, 0]])
+eigen_non_square = eigenValues(matrix_non_square)
+print("Singular values (non-persegi):", eigen_non_square)
