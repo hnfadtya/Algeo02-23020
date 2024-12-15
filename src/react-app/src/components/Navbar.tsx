240c04eca3ece1295dde5435c936bbb972562cf4
Abrar-Abhirama <13523038@std.stei.itb.ac.id>
2024-12-16 00:44:51 +0700

    be updated

--- src/react-app/src/components/Navbar.tsx
+++ src/react-app/src/components/Navbar.tsx
@@ -1,22 +1,34 @@
 import './Navbar.css';
 
-function Navbar() {
-  return (
-    <nav className="NavbarCustom">
-      {/* Nama Brand di kiri */}
-      <h1 className="NavbarBrand">EchoFinder</h1>
+interface NavbarProps {
+    onShowAlbum: () => void; // Fungsi untuk tombol Album
+    onShowMusic: () => void; // Fungsi untuk tombol Music
+}
 
-      {/* Tombol di kanan */}
-      <div className="NavbarButtons">
-        <a href="https://www.google.com/search?q=kucing+akmal&sca_esv=4a9e581ba0958f54&rlz=1C1UEAD_enID999ID999&udm=2&biw=960&bih=727&sxsrf=ADLYWILoFQpdq8hfR7UMZPKxXGuXKXpBcg%3A1733139503759&ei=L5xNZ6yGLoqhseMP-NqV2Ak&ved=0ahUKEwjs2d7j_4iKAxWKUGwGHXhtBZsQ4dUDCBA&uact=5&oq=kucing+akmal&gs_lp=EgNpbWciDGt1Y2luZyBha21hbDILEAAYgAQYsQMYgwEyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIEEAAYHjIEEAAYHjIEEAAYHjIEEAAYHkj6GlDwBFjhGHACeACQAQCYAcACoAG_F6oBBzAuOC40LjK4AQPIAQD4AQGYAg-gAuUWqAIKwgINEAAYgAQYsQMYQxiKBcICBhAAGAcYHsICChAAGIAEGEMYigXCAgQQIxgnwgIHECMYJxjqAsICCBAAGIAEGLEDwgIHEAAYgAQYCpgDB4gGAZIHBzIuNy4zLjOgB6pL&sclient=img">
-          <button className="NavbarButtonAlbum">Album</button>
-        </a>
-        <a href="https://www.google.com/search?q=kucing+akmal&sca_esv=4a9e581ba0958f54&rlz=1C1UEAD_enID999ID999&udm=2&biw=960&bih=727&sxsrf=ADLYWILoFQpdq8hfR7UMZPKxXGuXKXpBcg%3A1733139503759&ei=L5xNZ6yGLoqhseMP-NqV2Ak&ved=0ahUKEwjs2d7j_4iKAxWKUGwGHXhtBZsQ4dUDCBA&uact=5&oq=kucing+akmal&gs_lp=EgNpbWciDGt1Y2luZyBha21hbDILEAAYgAQYsQMYgwEyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIEEAAYHjIEEAAYHjIEEAAYHjIEEAAYHkj6GlDwBFjhGHACeACQAQCYAcACoAG_F6oBBzAuOC40LjK4AQPIAQD4AQGYAg-gAuUWqAIKwgINEAAYgAQYsQMYQxiKBcICBhAAGAcYHsICChAAGIAEGEMYigXCAgQQIxgnwgIHECMYJxjqAsICCBAAGIAEGLEDwgIHEAAYgAQYCpgDB4gGAZIHBzIuNy4zLjOgB6pL&sclient=img">
-        <button className="NavbarButtonMusic">Music</button>
-        </a>
-      </div>
-    </nav>
-  );
+function Navbar({ onShowAlbum, onShowMusic }: NavbarProps) {
+    return (
+        <nav className="NavbarCustom">
+            <h1 className="NavbarBrand">EchoFinder</h1>
+            <div className="NavbarButtons">
+                <button
+                    onClick={() => {
+                        console.log("Album button clicked"); // Debug
+                        onShowAlbum();
+                    }}
+                >
+                    Album
+                </button>
+                <button
+                    onClick={() => {
+                        console.log("Music button clicked"); // Debug
+                        onShowMusic();
+                    }}
+                >
+                    Music
+                </button>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
